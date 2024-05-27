02a4dd2860b6e7c2864cddd41ad532c1ec5a4e61
Jhojan Tobón <jhojantobonm@gmail.com>
2024-05-27 14:58:03 -0500

    Created About and Home pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,27 @@
+import { Link, NavLink, Route, Routes } from "react-router-dom";
+import About from "@/pages/About";
+import Home from "./pages/Home";
+
 export function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world! </h1>;
+  return (
+    <>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
+  );
 }
