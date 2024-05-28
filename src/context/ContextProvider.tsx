54e9c39daaa61ab340beb2a5a4b3bbe194542a2b
Jhojan Tobón <jhojantobonm@gmail.com>
2024-05-28 11:12:37 -0500

    Created general context.

---
+++ src/context/ContextProvider.tsx
@@ -0,0 +1,40 @@
+import { ReactNode, createContext, useContext, useState } from "react";
+
+const Context = createContext<Context>({
+  navbarItems: [],
+  openMobileMenu: false,
+  setOpenMobileMenu: () => {},
+});
+
+export function useContextApp() {
+  return useContext(Context);
+}
+
+export function ContextProvider({ children }: { children: ReactNode }) {
+  const [openMobileMenu, setOpenMobileMenu] = useState<boolean>(() => {
+    return false;
+  });
+
+  const navbarItems = [
+    {
+      title: "Home",
+      slug: "/",
+    },
+    {
+      title: "About",
+      slug: "/about",
+    },
+    {
+      title: "Contact",
+      slug: "/contact",
+    },
+  ];
+
+  return (
+    <Context.Provider
+      value={{ navbarItems, openMobileMenu, setOpenMobileMenu }}
+    >
+      {children}
+    </Context.Provider>
+  );
+}
