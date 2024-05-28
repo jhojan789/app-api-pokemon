f598d2ceaba40fa4867cc5b1e19e35d653aadabb
Jhojan Tobón <jhojantobonm@gmail.com>
2024-05-28 08:42:18 -0500

    Created pokemon api functions.

---
+++ src/api/pokemon.ts
@@ -0,0 +1,21 @@
+export async function getPokemonsAPI() {
+  try {
+    const response = await fetch(
+      "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0"
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getPokemonDetailByURL(url: string) {
+  try {
+    const response = await fetch(url);
+    const details = await response.json();
+    return details;
+  } catch (error) {
+    console.log(error);
+  }
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,42 @@
+import { getPokemonDetailByURL, getPokemonsAPI } from "@/api/pokemon";
 import { Card } from "@/components/Card";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  console.log(pokemons);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const pokemonsArray = [];
+
+        const response = await getPokemonsAPI();
+
+        for await (const pokemon of response.results) {
+          const pokemonDetails = await getPokemonDetailByURL(pokemon.url);
+          pokemonsArray.push({
+            id: pokemonDetails.id,
+            name: pokemonDetails.name,
+            type: pokemonDetails.types[0].type.name,
+            order: pokemonDetails.order,
+            image:
+              pokemonDetails.sprites.other["official-artwork"].front_default,
+            cries: pokemonDetails.latest,
+          });
+        }
+
+        setPokemons(pokemonsArray);
+      } catch (error) {}
+    })();
+  }, []);
+
   return (
     <main>
       <section>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {pokemons.map((pokemon) => (
+          <Card key={pokemon.id} pokemon={pokemon} />
+        ))}
       </section>
     </main>
   );
