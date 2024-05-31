f8c17026eea50f0eddea151e1b7891cd416f9fc4
Jhojan Tobón <jhojantobonm@gmail.com>
2024-05-31 17:32:50 -0500

    Created Skeleton component.

---
+++ src/components/Skeleton.tsx
@@ -0,0 +1,20 @@
+export function Skeleton({ qty }: { qty: number }) {
+  const cards = [];
+
+  for (let i = 1; i <= qty; i++) {
+    cards.push(<SkeletonCard />);
+  }
+
+  return cards;
+}
+function SkeletonCard() {
+  return (
+    <div
+      className="flex justify-start items-center gap-7
+ w-full max-w-96 min-h-36 border bg-slate-200 rounded-lg p-4 animate-pulse"
+    >
+      <div className=" w-28 h-28 bg-slate-400 rounded-lg animate-pulse"></div>
+      <div className=" w-40 h-10 bg-slate-400 rounded-lg animate-pulse"></div>
+    </div>
+  );
+}
