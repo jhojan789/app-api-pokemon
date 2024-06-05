c9b202fe6b0fa14d955c9caac1bbd387df23dccb
Jhojan Tobón <jhojantobonm@gmail.com>
2024-06-05 12:42:15 -0500

    Added global variable. - Added useLocation.

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,3 +1,9 @@
+import { useLocation } from "react-router-dom";
+
 export default function NotFound() {
-  return <div>NotFound</div>;
+  const location = useLocation();
+
+  console.log(location.state);
+
+  return <div>{location.state} Not Found </div>;
 }
