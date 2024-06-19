848eefca0dc0cf65eb694e2dbc86b3eaacbf96f8
Jhojan Tobón <jhojantobonm@gmail.com>
2024-06-18 19:25:57 -0500

    Added styles to the about page.

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,14 @@
 export default function About() {
-  return <div>About</div>;
+  return (
+    <main>
+      <h1 className="font-bold text-center">ABOUT</h1>
+      <div className="bg-blue-200 rounded-lg p-4 shadow-lg mt-2">
+        <p>
+          This project shows the pokemons by category as well as individually,
+          it fetches the pokemon api to present information of each one.
+        </p>
+        <p>Created by Jhojan Tobón</p>
+      </div>
+    </main>
+  );
 }
